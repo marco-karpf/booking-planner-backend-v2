77f059aaa532bad450bc26c135d103608dfe3d5b
Marco Karpf <marco.karpf@swisscom.com>
2022-11-22 08:32:55 +0100

    create booking service

---
+++ src/main/java/ch/bbw/km/service/BookingService.java
@@ -0,0 +1,93 @@
+package ch.bbw.km.service;
+
+import ch.bbw.km.model.Booking;
+import ch.bbw.km.model.BookingDate;
+import ch.bbw.km.model.Status;
+import ch.bbw.km.model.User;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.Formattable;
+import java.util.List;
+
+@ApplicationScoped
+public class BookingService {
+
+    public List<Booking> getAllBookings() {
+        return Booking.listAll();
+    }
+
+    public Booking getBookingById(Long id) {
+        return Booking.findById(id);
+    }
+
+    public List<Booking> getBookingsByTitleAndUsername(String title, String username) {
+        List<Booking> bookings = Booking.list("SELECT b.bookings FROM User b WHERE b.username = ?1", username);
+        bookings.removeIf(booking -> !booking.title.toLowerCase().contains(title.toLowerCase()));
+        return bookings;
+    }
+
+    public List<Booking> getBookingsByUsername(String username) {
+        return Booking.list("SELECT b.bookings FROM User b WHERE b.username = ?1", username);
+    }
+
+
+    @Transactional
+    public Booking createBooking(Booking booking) {
+        booking.status = Status.findById(1L);
+        booking.persist();
+        return booking;
+    }
+
+    @Transactional
+    public Booking updateBooking(Long id, Booking booking) {
+        Booking bookingToUpdate = getBookingById(id);
+        if (bookingToUpdate != null) {
+            for (BookingDate bookingDate : booking.bookingDates) {
+                BookingDate bookingDateToUpdate = BookingDate.findById(bookingDate.id);
+                bookingDateToUpdate.startDateTime = bookingDate.startDateTime;
+                bookingDateToUpdate.endDateTime = bookingDate.endDateTime;
+                bookingDateToUpdate.persist();
+            }
+            bookingToUpdate.title = booking.title;
+            bookingToUpdate.status = booking.status;
+            bookingToUpdate.persist();
+            return bookingToUpdate;
+        }
+        return null;
+    }
+
+    @Transactional
+    public Booking cancelBooking(User user, Long id) {
+        Booking bookingToUpdate = getBookingById(id);
+        if (bookingToUpdate != null) {
+            if (user.role.equals("ADMIN")) {
+                bookingToUpdate.status = Status.find("status", "cancelled").singleResult();
+                bookingToUpdate.persist();
+                return bookingToUpdate;
+            } else if (user.role.equals("MEMBER")) {
+                bookingToUpdate.status = Status.find("status", "requested for cancellation").singleResult();
+                bookingToUpdate.persist();
+                return bookingToUpdate;
+            }
+        }
+        return null;
+    }
+
+    @Transactional
+    public Booking confirmBooking(Long id) {
+        Booking bookingToUpdate = getBookingById(id);
+        bookingToUpdate.status = Status.find("status", "approved").singleResult();
+        bookingToUpdate.persist();
+        return bookingToUpdate;
+    }
+
+    @Transactional
+    public void deleteBooking(Long id) {
+        Booking booking = getBookingById(id);
+        booking.delete();
+    }
+
+}
