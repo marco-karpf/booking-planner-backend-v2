d7bb82630767f8c3ea5ad68c76b79706bf86904b
Marco Karpf <marco.karpf@swisscom.com>
2022-11-22 08:35:54 +0100

    add user tests

---
+++ src/test/java/ch/bbw/km/controller/UserResourceTest.java
@@ -0,0 +1,90 @@
+package ch.bbw.km.controller;
+
+import ch.bbw.km.model.User;
+import ch.bbw.km.security.JwtService;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+class UserResourceTest {
+
+    @Test
+    void getUserWithoutJWT() {
+        given()
+                .contentType("application/json")
+                .when().get("/users")
+                .then()
+                .statusCode(401);
+    }
+
+    @Test
+    void getUserWithJWT(){
+        User admin = User.find("username", "john").firstResult();
+        String jwt = new JwtService().generateJwt(admin);
+        given()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + jwt)
+                .when().get("/users")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getUserById() {
+        User admin = User.find("username", "john").firstResult();
+        String jwt = new JwtService().generateJwt(admin);
+        given()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + jwt)
+                .when().get("/users/2")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getUsersByUsername() {
+        User admin = User.find("username", "john").firstResult();
+        String jwt = new JwtService().generateJwt(admin);
+        given()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + jwt)
+                .when().get("/users/username/?username=jane")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void updateUser() {
+        User admin = User.find("username", "john").firstResult();
+        String jwt = new JwtService().generateJwt(admin);
+        User user = new User();
+        user.firstName = "Jane";
+        user.lastName = "Doe";
+        user.age = 20;
+        user.username = "jane";
+        user.password = "123";
+        user.email = "jane.doe@mail.com";
+        user.role = "MEMBER";
+        given()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + jwt)
+                .body(user)
+                .when().put("/users/2")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void deleteUser() {
+        User admin = User.find("username", "john").firstResult();
+        String jwt = new JwtService().generateJwt(admin);
+        given()
+                .contentType("application/json")
+                .header("Authorization", "Bearer " + jwt)
+                .when().delete("/users/2")
+                .then()
+                .statusCode(200);
+    }
+}
